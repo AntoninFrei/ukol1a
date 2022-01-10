7eaf243a3f632474480c3c58cd6f4ffe6604be26
Antonín Frei <antonin.frei@seznam.cz>
2022-01-10 19:11:51 +0100

    ukol 1a

---
+++ src/com/engeto/examples/Main.java
@@ -0,0 +1,19 @@
+package com.engeto.examples;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String nameOfClient;
+    int numberOfBuy;
+    String City;
+    double fuelConsumption;
+    BigDecimal lengthOfCable;
+    LocalDate dateOfBirth;
+    String SPZ;
+    int[] ipAddress;
+
+    }
+}
